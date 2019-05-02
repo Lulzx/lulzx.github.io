2bddb8ae6c3a95ae4fb751df1cbb0fee6958d95b
Lulzx <wiserishabh@gmail.com>
2019-05-03 01:47:08 +0530

    new portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+now = (new Date()).getFullYear();
+
+document.getElementById("age").innerHTML =
+    "aged " + (now - 2000);
+
+document.getElementById("programming").innerHTML =
+    "for " + (now - 2014) + " years";
