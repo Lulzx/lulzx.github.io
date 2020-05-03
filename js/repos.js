87434e8c21d2ccda8a92ceb0fd22a1d246f1983d
Lulzx <wiserishabh@gmail.com>
2020-05-03 16:57:17 +0530

    Update repos.js

--- js/repos.js
+++ js/repos.js
@@ -1,6 +1,6 @@
 $(function() {
-  var limit = 10;       // how many repos to list
-  var login = 'badboy'; // your username
+  var limit = 300;
+  var login = 'lulzx';
 
   $.getJSON('https://api.github.com/users/' + login + '/repos?sort=pushed&callback=?', function(data) {
     var repos = data.data
@@ -14,7 +14,7 @@ $(function() {
 
     $('#repos').text('');
     $.each(repos.slice(0, len), function(ind, val) {
-      if(val.name == 'badboy.github.com') return;
+      if(val.name == 'lulzx.github.io') return;
       if(ind+1 == len)
         $('#repos').append('<li><a href="' + val.html_url + '">&quot;' + val.name + '&quot;</a></li>');
       else
